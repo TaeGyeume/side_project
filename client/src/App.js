664bd8a3fe3def6852ad0cd2e2e2e9084b64ecca
MIN-des <lhm7386@gmail.com>
2025-01-06 19:35:59 +0900

    App.js 수정중

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,8 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Profile from "./pages/Profile"; // 내 정보 페이지
 import Login from "./pages/Login"; // 로그인 페이지
 import Register from "./components/Register"; // 회원가입 페이지
-import Login from "./components/Login";
 import UserList from "./components/UserList";
 import ChatRoom from "./components/ChatRoom";
-import AddUser from "./components/AddUser";
 
 const App = () => {
   const [token, setToken] = useState(localStorage.getItem("token"));
@@ -21,15 +19,15 @@ const App = () => {
     localStorage.removeItem("token"); // 토큰 제거
   };
 
-  const [currentUser, setCurrentUser] = useState(null);
+//   const [currentUser, setCurrentUser] = useState(null);
 
-  useEffect(() => {
-    const storedUser = JSON.parse(localStorage.getItem("currentUser"));
-    console.log("Stored user in localStorage:", storedUser); // 디버깅
-    if (storedUser) {
-      setCurrentUser(storedUser);
-    }
-  }, []);
+//   useEffect(() => {
+//     const storedUser = JSON.parse(localStorage.getItem("currentUser"));
+//     console.log("Stored user in localStorage:", storedUser); // 디버깅
+//     if (storedUser) {
+//       setCurrentUser(storedUser);
+//     }
+//   }, []);
 
 
     return (
@@ -69,8 +67,8 @@ const App = () => {
                     path="/"
                     element={<h1>메인페이지!</h1>} // 메인 페이지
                 />
-                <Route path="/messages" element={<UserList currentUser={currentUser} />} />
-                <Route path="/chat/:roomId" element={<ChatRoom currentUser={currentUser} />} />
+                {/* <Route path="/messages" element={<UserList currentUser={currentUser} />} />
+                <Route path="/chat/:roomId" element={<ChatRoom currentUser={currentUser} />} /> */}
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login onLogin={handleLogin} />} />
                 <Route
