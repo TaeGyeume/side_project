834f0529731f149afb218799ea765c5092b13f5e
kimminhyuck <rhkddka02@naver.com>
2025-02-06 19:17:22 +0900

    소셜 로그인 콜백 경로에서 불필요한 공백 제거

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import ForgotPassword from './pages/auth/ForgotPassword';
 import ResetPassword from './pages/auth/ResetPassword';
 import EditProfile from './pages/user/EditProfile';
 import Header from './components/Header';
-import NaverLoginCallback from './components/SocialLogin/NaverLoginCallback';  
+import NaverLoginCallback from './components/SocialLogin/NaverLoginCallback';
 import FacebookLoginCallback from './components/SocialLogin/FacebookLoginCallback';
 import KakaoLoginCallback from './components/SocialLogin/KakaoLoginCallback';
 import { useAuthStore } from './store/authStore'; // Zustand 스토어
@@ -75,8 +75,8 @@ const App = () => {
             element={isAuthenticated ? <Navigate to="/profile" /> : <AuthPages.Login />}
           />
           <Route path="/kakao/callback" element={<KakaoLoginCallback />} />
-          <Route path="/naver/callback" element={<NaverLoginCallback />} /> 
-          <Route path="/facebook/callback" element={<FacebookLoginCallback />} />  
+          <Route path="/naver/callback" element={<NaverLoginCallback />} />
+          <Route path="/facebook/callback" element={<FacebookLoginCallback />} />
           <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/accommodations/search" element={<AccommodationSearch />} />
