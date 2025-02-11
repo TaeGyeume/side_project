a5c31dbbc9bd27d1952e93c7d32b91f16c83bc34
TaeGyeume <crow0224@gmail.com>
2025-02-11 16:55:55 +0900

    왜 이래

--- client/src/components/flights/LoadingScreen.js
+++ client/src/components/flights/LoadingScreen.js
@@ -3,7 +3,7 @@ import './styles/LoadingScreen.css';
 
 const LoadingScreen = () => (
   <div className="loading-screen">
-    <img src="/images/screen/flightsloading2.gif" alt="Loading..." />
+    <img src="/images/screen/flightsloading.gif" alt="Loading..." />
     <p>검색 중...</p>
   </div>
 );
